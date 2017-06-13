2fee65611951f1baccc399f117a785fcc17a1880
DiRock <araujo.die93@gmail.com>
2017-06-13 12:23:31 -0300

    Extends na classe sombreamento

--- gulpfile.js
+++ gulpfile.js
@@ -2,8 +2,8 @@ var gulp = require("gulp"),
 	less = require("gulp-less");
 
 gulp.task("default", function(){
-	gulp.watch(['css/estilos.less']).on('change', function (event) {
-        gulp.src(event.path)
+	gulp.watch(['css/**/*.less']).on('change', function (event) {
+        gulp.src("css/estilos.less")
             .pipe(less().on('error', function (erro) {
                 console.log('Problema na compilação: '+erro.filename);
                 console.log(erro.message);
